d495359c950d43d0e25e7140f4121397de775c71
Guilherme Ferreira <136031870+GuilhermeF-R@users.noreply.github.com>
2024-02-05 10:07:24 -0300

    Update resolução.py

--- resolução.py
+++ resolução.py
@@ -5,5 +5,5 @@ buttonfantasma.config(background="#242424", highlightthickness=0, bd=0)
 
 perfil.mainloop()
 
-Ao introduzir um botão fantasma, conseguimos resolver o problema de exibição da imagem que anteriormente não estava sendo exibida na tela.
-Ao adicionar esse elemento invisível, a imagem passou a ser exibida corretamente, solucionando assim o bug de forma eficaz.
+#Ao introduzir um botão fantasma, conseguimos resolver o problema de exibição da imagem que anteriormente não estava sendo exibida na tela.
+#Ao adicionar esse elemento invisível, a imagem passou a ser exibida corretamente, solucionando assim o bug de forma eficaz.
